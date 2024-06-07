34e5faf4d6f596d59c3258e9f612b14adf89ddb2
Daniel Bernardi <danbernardi1@gmail.com>
2024-06-07 14:42:11 -0700

    Adds fallback for suspense loaders

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Filters from '@/components/dashboard/filters';
 import Header from '@/components/dashboard/header';
 import SideNav from '@/components/dashboard/sidenav';
+import { Loader } from '@/components/ui/loader';
 import { Suspense } from 'react';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -9,11 +10,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="max-w-[1440px] mx-auto px-4 md:px-10 z-10 relative">
       <Header />
       <div className="grid gap-x-3 md:grid-cols-[240px,1fr,240px] xl:grid-cols-[300px,1fr,300px]">
-        <Suspense>
+        <Suspense fallback={<Loader variant="blocks" theme="dark" />}>
           <SideNav />
         </Suspense>
         <div className="pt-3 d:py-3">{children}</div>
-        <Suspense>
+        <Suspense fallback={<Loader variant="blocks" theme="dark" />}>
           <Filters />
         </Suspense>
       </div>

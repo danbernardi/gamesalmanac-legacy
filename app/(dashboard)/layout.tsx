b8885f13f5aea182f8a479869dd2e264f9baf679
Daniel Bernardi <danbernardi1@gmail.com>
2024-06-08 19:57:02 -0700

    Adds scroll to top button

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,23 +1,26 @@
 import Filters from '@/components/dashboard/filters';
 import Header from '@/components/dashboard/header';
 import SideNav from '@/components/dashboard/sidenav';
-import { Loader } from '@/components/ui/loader';
+import ScrollToTopBtn from '@/components/ui/scroll-to-top-btn';
 import { Suspense } from 'react';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
-
   return (
     <div className="max-w-[1440px] mx-auto px-4 md:px-10 z-10 relative">
       <Header />
       <div className="grid gap-x-3 md:grid-cols-[240px,1fr,240px] xl:grid-cols-[300px,1fr,300px]">
-        <Suspense fallback={<Loader variant="blocks" theme="dark" />}>
+        <Suspense fallback={<></>}>
           <SideNav />
         </Suspense>
         <div className="pt-3 d:py-3">{children}</div>
-        <Suspense fallback={<Loader variant="blocks" theme="dark" />}>
+        <Suspense fallback={<></>}>
           <Filters />
         </Suspense>
       </div>
+
+      <Suspense fallback={<></>}>
+        <ScrollToTopBtn />
+      </Suspense>
     </div>
   );
 }

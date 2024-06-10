3f965567d9272c847cf79c8ff8440b1ccb3f2f50
Daniel Bernardi <danbernardi1@gmail.com>
2024-06-09 17:39:34 -0700

    Adds header link

--- app/components/dashboard/header.tsx
+++ app/components/dashboard/header.tsx
@@ -1,19 +1,23 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from 'next/image';
 import SearchForm from "./search-form";
 import { Suspense } from "react";
+import Link from "next/link";
 
 export default function Header() {
+  const now = new Date();
+
   return (
     <header className="flex flex-col sm:flex-row h-full mt-3 pt-3 md:py-3 text-primary-foreground justify-between sm:items-center">
-      <Image
-        className="w-[240px] lg:w-[300px]"
-        src="/gamesalmanac_logo.svg"
-        alt="Games Almanac"
-        width={300}
-        height={32}
-        priority
-      />
+      <Link href={`/${now.getMonth() + 1}/${now.getFullYear()}`}>
+        <Image
+          className="w-[240px] lg:w-[300px]"
+          src="/gamesalmanac_logo.svg"
+          alt="Games Almanac"
+          width={300}
+          height={32}
+          priority
+        />
+      </Link>
 
       <Suspense fallback={(<></>)}>
         <SearchForm />
